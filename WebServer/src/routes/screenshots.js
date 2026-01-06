4fa473b0bd6c9fa7a6d570a3747084600d994fab
Amandine L <amandine.langlais03@gmail.com>
2026-01-06 18:08:54 +0100

    Feat : Change pico2wave to espeak

--- WebServer/src/routes/screenshots.js
+++ WebServer/src/routes/screenshots.js
@@ -10,85 +10,84 @@ const router = require('express').Router(),
   ScreenshotsUtil = require('../modules/ScreenshotsUtil');
 
 router.get('/', (req, res, next) => {
-    res.render('screenshots');
-  }
+  res.render('screenshots');
+}
 );
 
 router.get('/snap', (req, res, next) => {
-    let newId = ScreenshotsUtil.getNewId();
-    console.log("screenid : " + newId);
-    console.log(config.buildPath + 'ScreenshotUtil/');
-    child_process.execSync('sudo ./screenshotUtil -id ' + newId, {
-      cwd: (config.buildPath + 'ScreenshotUtil/')
-    });
-    let scrData = ScreenshotsUtil.getData(newId);
-    let data = JSON.parse(fs.readFileSync(config.dataPath + 'unreviewedRois.json'));
-    data["index"] = data["index"].concat(scrData["rois_id"]);
-    fs.writeFileSync(config.dataPath + 'unreviewedRois.json', JSON.stringify(data, null, 2));
-    res.render('screenshots', {
-      screenPicPath: scrData.undist_lightOn
-    });
-  }
+  let newId = ScreenshotsUtil.getNewId();
+  console.log("screenid : " + newId);
+  console.log(config.buildPath + 'ScreenshotUtil/');
+  child_process.execSync('sudo ./screenshotUtil -id ' + newId, {
+    cwd: (config.buildPath + 'ScreenshotUtil/')
+  });
+  let scrData = ScreenshotsUtil.getData(newId);
+  let data = JSON.parse(fs.readFileSync(config.dataPath + 'unreviewedRois.json'));
+  data["index"] = data["index"].concat(scrData["rois_id"]);
+  fs.writeFileSync(config.dataPath + 'unreviewedRois.json', JSON.stringify(data, null, 2));
+  res.render('screenshots', {
+    screenPicPath: scrData.undist_lightOn
+  });
+}
 );
 
 router.get('/review', (req, res, next) => {
   let data = JSON.parse(fs.readFileSync(config.dataPath + 'unreviewedRois.json'));
   let roiToReview = [];
-  for(let id of data["index"])
+  for (let id of data["index"])
     roiToReview.push(RoisUtil.getData(id));
   res.render('review', {
     rois: roiToReview,
     objs: ObjectsUtil.getAll()
   });
-  }
+}
 );
 
 router.post('/review', (req, res, next) => {
   let data = JSON.parse(fs.readFileSync(config.dataPath + 'unreviewedRois.json'));
-  for(let roiId in req.body) {
+  for (let roiId in req.body) {
     let val = req.body[roiId];
     if (val == "dismiss")
       RoisUtil.deleteRoi(id);
-    else if(val != "todo")
-      RoisUtil.serializeData(RoisUtil.setClass(RoisUtil.getData(roiId),val));
+    else if (val != "todo")
+      RoisUtil.serializeData(RoisUtil.setClass(RoisUtil.getData(roiId), val));
 
-    if(val != "todo") {
+    if (val != "todo") {
       let index = data["index"].indexOf(id);
       data["index"].splice(index, 1);
     }
   }
   fs.writeFileSync(config.dataPath + 'unreviewedRois.json', JSON.stringify(data, null, 2));
-    res.render('screenshots');
-  }
+  res.render('screenshots');
+}
 );
 
 router.get('/predict', (req, res, next) => {
-    let newId = ScreenshotsUtil.getNewId();
-    console.log("screenid : " + newId);
-    child_process.execSync('sudo ./screenshotUtil -id ' + newId, {
-      cwd: (config.buildPath + 'ScreenshotUtil/')
+  let newId = ScreenshotsUtil.getNewId();
+  console.log("screenid : " + newId);
+  child_process.execSync('sudo ./screenshotUtil -id ' + newId, {
+    cwd: (config.buildPath + 'ScreenshotUtil/')
+  });
+  let scrData = ScreenshotsUtil.getData(newId);
+  let predictionRslts = {};
+  for (let roisId of scrData["rois_id"]) {
+    let strClassRslt = child_process.execSync('./predictUtil -id ' + roisId, {
+      cwd: (config.buildPath + 'PredictUtil/')
     });
-    let scrData = ScreenshotsUtil.getData(newId);
-    let predictionRslts = {};
-    for(let roisId of scrData["rois_id"]) {
-      let strClassRslt = child_process.execSync('./predictUtil -id ' + roisId, {
-        cwd: (config.buildPath + 'PredictUtil/')
-      });
-      if(!(strClassRslt in predictionRslts))
-        predictionRslts[strClassRslt] = 0;
-      predictionRslts[strClassRslt] += 1;
-    }
-    let audioOutput = '\"Il y a ';
-    for(let classId in predictionRslts) {
-      let objData = ObjectsUtil.getData(parseInt(classId));
-      audioOutput += (predictionRslts[classId] + ' ' + objData["audio_fr"] + ", ");
-    }
-    audioOutput += '\"';
-    child_process.execSync('pico2wave -l fr-FR -w tts.wav ' + audioOutput + " && aplay tts.wav && rm tts.wav", {
-      cwd: config.dataPath
-    });
-    res.redirect('/screenshots/');
+    if (!(strClassRslt in predictionRslts))
+      predictionRslts[strClassRslt] = 0;
+    predictionRslts[strClassRslt] += 1;
   }
+  let audioOutput = '\"Il y a ';
+  for (let classId in predictionRslts) {
+    let objData = ObjectsUtil.getData(parseInt(classId));
+    audioOutput += (predictionRslts[classId] + ' ' + objData["audio_fr"] + ", ");
+  }
+  audioOutput += '\"';
+  child_process.spawnSync("espeak-ng", ["-v", "fr", audioOutput]);
+
+  res.redirect('/screenshots/');
+}
 );
 
 module.exports = router;
